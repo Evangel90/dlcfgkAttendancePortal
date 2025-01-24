77805522468c20e014ff8f31a8022eda7b888d43
Evangel90 <oladipoevangel90@gmail.com>
2025-01-24 21:33:27 +0100

    added middleware to attendanceRoutes

--- backend/src/routes/attendanceRoutes.ts
+++ backend/src/routes/attendanceRoutes.ts
@@ -6,13 +6,15 @@ import {
     updateAttendance,
     deleteAttendance, 
 } from '../controllers/attendanceController';
+import authMiddleware from '../middlewares/authMiddleware';
+import roleMiddleware from '../middlewares/roleMiddleware';
 
 const attendanceRouter = express.Router();
 
-attendanceRouter.get('/getAttendance', getAllAttendance);
-attendanceRouter.get('/getAttendanceById', getAttendanceById);
-attendanceRouter.post('/createAttendance', createAttendance);
-attendanceRouter.patch('/updateAttendance/:id', updateAttendance);
-attendanceRouter.delete('/deleteAttendance/:id', deleteAttendance);
+attendanceRouter.get('/getAttendance', authMiddleware, getAllAttendance);
+attendanceRouter.get('/getAttendanceById', authMiddleware, getAttendanceById);
+attendanceRouter.post('/createAttendance', authMiddleware, roleMiddleware, createAttendance);
+attendanceRouter.patch('/updateAttendance/:id', authMiddleware, roleMiddleware, updateAttendance);
+attendanceRouter.delete('/deleteAttendance/:id', authMiddleware, roleMiddleware, deleteAttendance);
 
 export default attendanceRouter;
